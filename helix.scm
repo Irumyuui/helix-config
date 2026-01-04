498872bc05e9c521443ba4f92a38170aae5018f5
中宮千尋 <ciel_katana@outlook.com>
2026-01-05 00:29:19 +0800

    add helix scm open cmd

--- helix.scm
+++ helix.scm
@@ -1,17 +1,18 @@
-; (require "helix/editor.scm")
-; (require (prefix-in helix. "helix/commands.scm"))
-; (require (prefix-in helix.static. "helix/static.scm"))
+(require (prefix-in helix. "helix/commands.scm"))
+(require (prefix-in helix.static. "helix/static.scm"))
 
-; (provide open-helix-scm open-init-scm)
+(require "helix/editor.scm")
+(require "helix/misc.scm")
 
-; ;;@doc
-; ;; Open the helix.scm file
-; (define (open-helix-scm)
-;   (helix.open (helix.static.get-helix-scm-path)))
+(provide helix-scm-open init-scm-open)
 
-; ;;@doc
-; ;; Opens the init.scm file
-; (define (open-init-scm)
-;   (helix.open (helix.static.get-init-scm-path)))
-  
-	
+;;@doc
+;; Open the helix.scm file
+(define (helix-scm-open)
+  (helix.open (helix.static.get-helix-scm-path)))
+
+;;@doc
+;; Opens the init.scm file
+(define (init-scm-open)
+  (helix.open (helix.static.get-init-scm-path)))
+
